186ca0995d7f49c0563d93f9192f6a467c1c4874
Anamul10 <anamul.hasan@mnsu.edu>
2022-09-26 16:23:19 -0400

    added cart component and some styles

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({ cart }) => {
     return (
-        <div>
+        <div className='cart'>
             <h4>Order Summary</h4>
             <p>Selected Items: {cart.length}</p>
+            <p>Total price: </p>
+            <p>Total Shipping: </p>
+            <p>Tax: </p>
+            <h5>Grand Total: </h5>
         </div>
     );
 };
